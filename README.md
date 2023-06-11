efe4a2c301c1a75efdaff9b76591dee3d2003b34
Satyen Sen <Satyendra.sant7@gmail.com>
2023-06-11 23:55:51 +0530

    project initiation

---
+++ README.md
@@ -0,0 +1,28 @@
+# **Auto Drive**
+
+## Modern Car Rental Web app, built using Next.js 13, TypeScript & Tailwind CSS.
+
+---
+
+### Technologies Used
+
+-   React.js 18
+-   Next.js 13
+-   JavaScript (ECMAScript 2020)
+-   TypeScript 5
+-   Tailwind CSS 3
+-   Rpid Api - https://rapidapi.com/apininjas/api/cars-by-api-ninjas
+
+---
+
+### Starting Development Server
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) in your browser.
---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    Find, book, or rent a car - quickly and easily
+                </h1>
+                <p className='hero__subtitle'>
+                    Stremline your car rental experince with our effortless
+                    booking process
+                </p>
+            </div>
+        </div>
+    )
+}
